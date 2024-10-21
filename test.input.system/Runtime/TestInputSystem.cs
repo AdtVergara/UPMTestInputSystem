9ee56f634d4320e5568059e38ecaf51e657d4f4c
AdtVergara <adtvergara@gmail.com>
2024-10-21 17:03:14 +0200

    Update TestInputSystem.cs

--- test.input.system/Runtime/TestInputSystem.cs
+++ test.input.system/Runtime/TestInputSystem.cs
@@ -1,8 +1,32 @@
 #if ENABLE_INPUT_SYSTEM
 using UnityEngine.InputSystem;
+using System.Collections.Generic;
 
-class TestInputSystem
+public class TestInputSystem
 {
-
+    public string[] GetDevices()
+    {
+        var devices = new List<string>();
+        var inputDevices = InputSystem.devices;
+        foreach (var inputDevice in inputDevices)
+        {
+            switch (inputDevice)
+            {
+                case Keyboard keyboard:
+                    devices.Add(keyboard.description.ToString());
+                    break;
+                case Mouse mouse:
+                    devices.Add(mouse.description.ToString());
+                    break;
+                case Gamepad _:
+                case Joystick _:
+                    break;
+                case Touchscreen touchScreen:
+                    devices.Add(touchScreen.description.ToString());
+                    break;
+            }
+        }
+        return devices.ToArray();
+    }
 }
 #endif
